0e43ae3cc4c82da865179169c376fe1cb6195e44
GouravisCoding <pythonicgourav@gmail.com>
2024-07-03 21:48:51 +0530

    removed some commented out code

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -57,50 +57,6 @@ export default function NavBar() {
           {authStatus && <Logout />}
         </div>
       </nav>
-      {/* <nav
-        className={`${
-          menuOn ? "w-1/2" : "w-12"
-        } md:w-1/5 text-center text-2xl fixed top-0 left-0 right-0 py-10  bg-slate-800 text-white min-h-screen z-50`}
-      >
-        <div>
-          <h1
-            className={`font-bold md:inline-block p-2 ${
-              !menuOn ? "hidden" : ""
-            }`}
-          >
-            HeyThere
-          </h1>
-          <span
-            onClick={() => setMenuOn((prev) => !prev)}
-            className="float-right absolute top-2 right-2 md:hidden"
-          >
-            <Menu />
-          </span>
-        </div>
-        <div className="w-full flex flex-col">
-          {navItems.map(
-            (item) =>
-              item.auth === authStatus && (
-                <NavLink
-                  className={({ isActive }) =>
-                    `p-2 mx-2 md:inline-block rounded-3xl ${
-                      isActive ? "bg-purple-600" : ""
-                    } ${!menuOn ? "hidden" : ""}`
-                  }
-                  key={item.id}
-                  to={item.to}
-                >
-                  {item.name}
-                </NavLink>
-              )
-          )}
-          {authStatus && (
-            <div className={`md:inline-block ${!menuOn ? "hidden" : ""}`}>
-              <Logout />
-            </div>
-          )}
-        </div>
-      </nav> */}
     </>
   );
 }
